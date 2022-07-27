f58643996cf48eb57ffc5a5e43bc21bd4d7d0455
Valentina Morales Callejas <vmorales@INTELIBPO.local>
2022-07-27 14:47:03 -0500

    soluciones

--- problems/problem_04.py
+++ problems/problem_04.py
@@ -3,4 +3,19 @@
     Ejemplo: si n = 100 que sume la siguiente lista: 3, 6, 9, 12..., 99
 '''
 def sum_multiples_of_three(number):
-    pass
+    if number == str (number):
+        return 0
+    elif number < 0:
+        x=-3
+        contador = 0
+        while x >= number:
+            contador= x + contador
+            x=x - 3
+        return contador
+    else: 
+        x=3
+        contador = 0
+        while x <= number:
+            contador= x + contador
+            x=x + 3
+        return contador
